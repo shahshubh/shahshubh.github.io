3ba5da6298600440073c694ac71c09a54fe54374
Grace-Rasaily780 <59549792+Grace-Rasaily780@users.noreply.github.com>
2020-10-01 15:34:07 +0545

    Logo added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,16 +6,15 @@ class About extends React.Component{
         super();
         this.state = {
             skills:[
-                {id:"HTML_CSS_skill", content: "HTML / CSS",porcentage: "85%", value: "85"},
-                
-                {id:"NodeJS_skill", content: "NodeJS / ExpressJS",porcentage: "75%", value: "75"},
-                {id:"MongoDB_skill", content: "MongoDB",porcentage: "75%", value: "75"},
-                {id:"ReactJS_skill", content: "ReactJS",porcentage: "75%", value: "75"},
-                {id:"ReactNative_skill", content: "React Native",porcentage: "70%", value: "70"},
-                {id:"JavaScript_skill", content: "JavaScript",porcentage: "70%", value: "70"},
-                {id:"Java_skill", content: "Java",porcentage: "70%", value: "70"},
-                {id:"Python_skill", content: "Python",porcentage: "40%", value: "40"},
-                {id:"MachineLearning_skill", content: "Machine Learning",porcentage: "40%", value: "40"},
+                {id:"HTML_CSS_skill", content: "HTML / CSS",porcentage: "85%", value: "85", logo: "devicon-html5-plain-wordmark colored"},
+                {id:"NodeJS_skill", content: "NodeJS / ExpressJS",porcentage: "75%", value: "75", logo: "devicon-nodejs-plain colored"},
+                {id:"MongoDB_skill", content: "MongoDB",porcentage: "75%", value: "75", logo: "devicon-mongodb-plain-wordmark colored"},
+                {id:"ReactJS_skill", content: "ReactJS",porcentage: "75%", value: "75", logo: "devicon-react-original colored"},
+                {id:"ReactNative_skill", content: "React Native",porcentage: "70%", value: "70", logo: "devicon-react-original colored"},
+                {id:"JavaScript_skill", content: "JavaScript",porcentage: "70%", value: "70", logo: "devicon-javascript-plain colored"},
+                {id:"Java_skill", content: "Java",porcentage: "70%", value: "70", logo: "devicon-java-plain colored"},
+                {id:"Python_skill", content: "Python",porcentage: "40%", value: "40", logo: "devicon-python-plain colored"},
+                {id:"MachineLearning_skill", content: "Machine Learning",porcentage: "40%", value: "40", logo: "devicon-python-plain colored"},
             ],
             about_me:[
                 {id: "first-p-about",content: "I am a third year student in Computer Science and Engineering(CSE) who loves to transform ideas into reality using code. I am passionate about using Javascript."},
@@ -77,7 +76,7 @@ class About extends React.Component{
                                                 this.state.skills.map(skill => {
                                                     return(
                                                         <React.Fragment key={skill.id}>
-                                                            <span>{skill.content}</span> <span className="pull-right">{skill.porcentage}</span>
+                                                            <i className={skill.logo}></i> <span>{skill.content}</span> <span className="pull-right">{skill.porcentage}</span>
                                                             <div className="progress">
                                                                 <div className="progress-bar" role="progressbar" style={{width: skill.porcentage}} aria-valuenow={skill.value} aria-valuemin="0" aria-valuemax="100"></div>
                                                             </div>
